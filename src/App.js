54fd785efce5ae25fd56c2140028ce331644fd9d
abeez07 <abrahamyusuf17@gmail.com>
2023-10-19 23:07:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import bgpattern from './bg-pattern-desktop.svg'
+import logo from './logo.svg'
+import hero from './hero-desktop.jpg'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div class="main-container">
+        <div class="right-container">
+            <img src={bgpattern}alt=""/>
+            <img src={logo}alt="" id="top-left"/>
+            <h2 class="middle-left"> <p class="middle-top-left">W E'R E</p><br/>
+             <p class="middle-bottom-left">C O M I N G <br/> S O O N </p></h2>
+            <p class="note">Hello fellow shoppers! We're currently building our new <br/>fashion store. Add your email below to stay up-to-date with <br/> announcements and our launch details</p>
+        </div>
+             <img src={hero} alt=""/>
     </div>
-  );
+   </>
+  )
 }
 
-export default App;
+export default App
